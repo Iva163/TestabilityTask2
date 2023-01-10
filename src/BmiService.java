3a11ca12c0c6bd19441efe14f1be519ad1fc9e8f
Ivan Borisov <ivan_borisov_88@vk.com>
2023-01-10 19:28:20 +0400

    change

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public double calculate (double weight, double height) {
-        double massIndex = weight/(height*height);
+    public double calculate(double weight, double height) {
+        double massIndex = weight / (height * height);
         return massIndex;
 
     }
